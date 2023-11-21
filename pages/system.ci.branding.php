f37d34b5f8146d0c407fd650017677f6c3fa5ef5
Alexander Walther <mail@alexplus.de>
2023-11-21 15:25:39 +0100

    wording

--- pages/system.ci.branding.php
+++ pages/system.ci.branding.php
@@ -110,7 +110,7 @@ $fragment->setVar('body', $form->get(), false);
     <div class="col-lg-4">
         <?php
 
-$anchor = '<a target="_blank" href="https://donate.alexplus.de/?addon=be_style_fluent"><img src="'.rex_url::addonAssets('be_style_fluent', 'jetzt-spenden.svg').'" style="width: 100% max-width: 400px;"></a>';
+$anchor = '<a target="_blank" href="https://donate.alexplus.de/?addon=be_style_fluent"><img src="'.rex_url::addonAssets('be_style_fluent', 'jetzt-beauftragen.svg').'" style="width: 100% max-width: 400px;"></a>';
 
 $fragment = new rex_fragment();
 $fragment->setVar('class', 'info', false);
